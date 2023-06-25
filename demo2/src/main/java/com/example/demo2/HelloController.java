45ac853adec8c2ee66859ef98a62f3ca377583df
Владислав Матвиенко <114211276+vladmatvienko@users.noreply.github.com>
2023-06-25 20:35:22 +0300

    Delete demo2 directory

--- demo2/src/main/java/com/example/demo2/HelloController.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo2;
-import javafx.fxml.FXML;
-public class HelloController {
-    @FXML
-    protected void onHelloButtonClick() {
-       HelloApplication.switchToNewWindow("SignIn");
-    }
-}
