13752c655a1a568ad625a78334dc0feb56502911
Владислав Матвиенко <114211276+vladmatvienko@users.noreply.github.com>
2023-06-25 20:37:58 +0300

    Delete Configs.java

--- Configs.java
+++
@@ -1,9 +0,0 @@
-package com.example.adm_it;
-
-public class Configs {
-    protected String dbHost = "localhost";
-    protected String dbPort = "3306";
-    protected String dbUser = "root";
-    protected String dbPass = "root";
-    protected String dbName = "1";
-}
